29f5ca698e1de45ab55753cb499272432c7d202a
Utsav Deep <utsavdeep01@gmail.com>
2022-08-27 20:42:41 +0530

    Netlify Plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import {
+  defineConfig
+} from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  output: 'server',
+  adapter: netlify(),
 });
