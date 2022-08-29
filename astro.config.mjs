8cdb2e0c172b4ba4237a642fafad6221f343596f
Utsav Deep <utsavdeep01@gmail.com>
2022-08-29 17:16:04 +0530

    Add prefetch and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,25 @@ import {
   defineConfig
 } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
-
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  site: 'https://utsavdeep.com/',
+  integrations: [svelte(), tailwind(), prefetch({
+    selector: "a[href^='/']",
+    selector: "a[href^='/project']",
+    selector: "a[href^='/contact']",
+    selector: "a[href^='/work']"
+  }), sitemap({
+    customPages: ['https://utsavdeep.com/', 'https://utsavdeep.com/work', 'https://utsavdeep.com/project', 'https://utsavdeep.com/contact'],
+    changefreq: 'weekly',
+    priority: 0.7,
+  })],
   output: 'server',
   adapter: netlify(),
   server: {
