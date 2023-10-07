68bb439de51880db9a18152526a21a1ba8a9006b
Utsav <utsavdeep01@gmail.com>
2023-10-07 23:40:37 +0530

    Update cache directory

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -2,7 +2,7 @@ import EleventyFetch from '@11ty/eleventy-fetch';
 
 const data = await EleventyFetch('https://gitconnected.com/v1/portfolio/pinnheads', {
     duration: '4w',
-    directory: './tmp/.cache',
+    directory: '.cache',
     type: 'json',
 });
 
