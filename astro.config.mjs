8ef2af01a208ac85e3b443698de950eb6ce20831
Utsav <utsavdeep01@gmail.com>
2023-10-08 00:54:01 +0530

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,21 +10,32 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://utsavdeep.com/',
-  integrations: [svelte(), tailwind(), prefetch({
-    selector: "a[href^='/']",
-    selector: "a[href^='/project']",
-    selector: "a[href^='/contact']",
-    selector: "a[href^='/work']"
-  }), sitemap({
-    customPages: ['https://utsavdeep.com/', 'https://utsavdeep.com/work', 'https://utsavdeep.com/project', 'https://utsavdeep.com/contact'],
-    changefreq: 'weekly',
-    priority: 0.7,
-  })],
-  output: 'server',
-  adapter: netlify(),
-  server: {
-    port: 4321,
-    host: true
-  }
+    site: 'https://utsavdeep.com/',
+    integrations: [
+        svelte(),
+        tailwind(),
+        prefetch({
+            selector: "a[href^='/']",
+            selector: "a[href^='/project']",
+            selector: "a[href^='/contact']",
+            selector: "a[href^='/work']",
+        }),
+        sitemap({
+            customPages: [
+                'https://utsavdeep.com/',
+                'https://utsavdeep.com/work',
+                'https://utsavdeep.com/project',
+                'https://utsavdeep.com/contact',
+            ],
+            changefreq: 'weekly',
+            priority: 0.7,
+        }),
+    ],
+    output: 'server',
+    adapter: netlify(),
+    server: {
+        port: 4321,
+        host: true,
+    },
+    cacheDir: '.cache',
 });
