4dcd77b7851b450fbc83930e3c1132bc8f5d0b9d
Utsav <utsavdeep01@gmail.com>
2023-10-11 00:02:41 +0530

    Update middleware onRequest Function

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,10 @@
 import EleventyFetch from "@11ty/eleventy-fetch";
 
-export async function onRequest({locals, request}, next) {
+export function onRequest({locals, request}, next) {
     let url = "https://gitconnected.com/v1/portfolio/pinnheads";
 
     /* This returns a promise */
-    const data = await EleventyFetch(url, {
+    const data = EleventyFetch(url, {
             duration: "4w", // save for 1 day
             directory: 'dist/cache',
             type: "json"    // we’ll parse JSON for you
